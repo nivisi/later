904cfb6fb451d8122b48b6614890f132c8f0c98f
Dima Kharitonov <hardi.programm@gmail.com>
2022-12-24 22:47:45 +0300

    add to app.dart theme classes

--- later/lib/presentation/later_app.dart
+++ later/lib/presentation/later_app.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 
 import 'navigation/later_router.dart';
 
+import 'package:later/presentation/theme/custom_colors.dart';
+import 'package:later/presentation/theme/custom_text_style.dart';
+
 class LaterApp extends StatelessWidget {
   LaterApp({super.key});
 
@@ -13,9 +16,10 @@ class LaterApp extends StatelessWidget {
       title: 'later',
       routerDelegate: _router.delegate(),
       routeInformationParser: _router.defaultRouteParser(),
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: ThemeData().copyWith(extensions: <ThemeExtension<dynamic>>[
+        CustomTextStyle(),
+        CustomColors(),
+      ]),
     );
   }
 }
