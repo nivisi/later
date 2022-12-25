676964be058958990ea7f96405e3a0156676fb04
Dima Kharitonov <hardi.programm@gmail.com>
2022-12-25 03:31:26 +0300

    finish RecordModel

--- later/lib/database/models/record_model.dart
+++ later/lib/database/models/record_model.dart
@@ -4,10 +4,21 @@ part 'record_model.g.dart';
 
 @collection
 class RecordModel {
+  RecordModel({
+    this.title,
+    this.description,
+    required this.url,
+    required this.lastEditedAt,
+  }) : createdAt = DateTime.now();
+
+  final String url;
+  final String? description;
+  final DateTime createdAt;
+  final DateTime lastEditedAt;
   Id id = Isar.autoIncrement; // you can also use id = null to auto increment
 
   @Index(type: IndexType.value)
-  String title = '';
+  String? title;
 
   @enumerated
   RecordStatus status = RecordStatus.later;
