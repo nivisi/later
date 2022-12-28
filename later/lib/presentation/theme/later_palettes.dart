1bac6c92d7900573b0a555e8c459bc383eebde21
Dima Kharitonov <hardi.programm@gmail.com>
2022-12-28 21:43:58 +0200

    create color palettes class

---
+++ later/lib/presentation/theme/later_palettes.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+abstract class LaterLightPalette {
+  const LaterLightPalette._();
+
+  static const backgroundColor = Colors.white;
+  static const backgroundSecondaryColor = Color(0xFFF2F2F2);
+  static const foregroundColor = Color(0xFF1E1E1E);
+  static const foregroundDisabledColor = Color(0xFFA5A5A5);
+  static const foregroundSecondaryColor = Color(0xFF888888);
+  static const foregroundSeocndaryDisabledColor = Color(0xFFC7C7C7);
+  static const primaryColor = Color(0xFFEF476F);
+  static const primaryDisabledColor = Color(0xFFF47C98);
+  static const secondaryPrimaryColor = Color(0xFF6A687A);
+  static const secondaryPrimaryDisabledColor = Color(0xFF728597);
+}
