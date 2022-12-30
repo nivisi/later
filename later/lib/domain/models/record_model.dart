516c9bac9092f656a8e5b2ce75d9e9b59ad021e8
Dima Kharitonov <hardi.programm@gmail.com>
2022-12-30 21:39:26 +0200

    create RecordModel in domain layer

---
+++ later/lib/domain/models/record_model.dart
@@ -0,0 +1,17 @@
+class RecordModel {
+  const RecordModel({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.url,
+    required this.lastEditedAt,
+    required this.createdAt,
+  });
+
+  final int id;
+  final String title;
+  final String description;
+  final String url;
+  final DateTime createdAt;
+  final DateTime lastEditedAt;
+}
