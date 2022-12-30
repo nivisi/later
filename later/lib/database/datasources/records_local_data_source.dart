c1a751bcb6cfaeb7d4d232400f975442ef4526b1
Dima Kharitonov <hardi.programm@gmail.com>
2022-12-30 23:39:30 +0200

    implementing abstract class

---
+++ later/lib/database/datasources/records_local_data_source.dart
@@ -0,0 +1,53 @@
+import 'package:injectable/injectable.dart';
+import 'package:isar/isar.dart';
+import 'package:later/database/models/record_model.dart';
+
+import './records_data_source.dart';
+
+class RecordsLocalDataSource extends RecordsDataSource {
+  late final Isar _isar;
+
+  // We must preinitialize it first
+  @FactoryMethod(preResolve: true)
+  static Future<RecordsLocalDataSource> init() async {
+    final repo = RecordsLocalDataSource()
+      .._isar = await Isar.open([RecordDbModelSchema]);
+
+    return repo;
+  }
+
+  @override
+  Future<RecordDbModel> create({
+    required String url,
+    String? title,
+    String? description,
+  }) async {
+    if (url.isEmpty) {
+      throw const FormatException('Page adress cannot be empty');
+    }
+
+    final record = RecordDbModel(
+      url: url,
+      createdAt: DateTime.now(),
+      lastEditedAt: DateTime.now(),
+    )
+      ..title = title
+      ..description = description;
+
+    await _isar.writeTxn(
+      () => _isar.recordDbModels.put(record), // insert & updat
+    );
+
+    return record;
+  }
+
+  @override
+  Future<RecordDbModel?> getById(int id) async {
+    return await _isar.writeTxn(() => _isar.recordDbModels.get(id));
+  }
+
+  @override
+  Stream<List<RecordDbModel>> watchAll() {
+    return _isar.recordDbModels.where().watch(fireImmediately: true);
+  }
+}
