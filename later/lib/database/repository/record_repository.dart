6cd424489be1afd8b9979377545259691bda22fe
Dima Kharitonov <hardi.programm@gmail.com>
2022-12-31 19:35:23 +0200

    implement RecordRepository on data layer

--- later/lib/database/repository/record_repository.dart
+++ later/lib/database/repository/record_repository.dart
@@ -1,4 +1,50 @@
 import 'package:injectable/injectable.dart';
+import 'package:later/database/datasources/records_data_source.dart';
+import 'package:later/database/models/record_model.dart';
+import 'package:later/domain/models/record_model.dart';
+import 'package:later/domain/repository/record_repository.dart';
 
 @lazySingleton
-class RecordRepository {}
+class RecordRepositoryImpl implements RecordsRepository {
+  RecordRepositoryImpl({
+    required this.recordsDataSurce,
+  });
+
+  final RecordsDataSource recordsDataSurce;
+
+  @override
+  Future<void> create({
+    required String? url,
+    required String title,
+    String? description,
+    DateTime? lastEditedAt,
+  }) async {
+    try {
+      if (title.isNotEmpty) {
+        await recordsDataSurce.create(
+          url: url,
+          title: title,
+          description: description,
+          lastEditedAt: lastEditedAt,
+        );
+      }
+    } catch (_) {
+      throw Exception();
+    }
+  }
+
+  @override
+  Future<RecordModel?> getById(int id) async {
+    // final newModel = await recordsDataSurce.getById(id);
+  }
+
+  Future<RecordDbModel> _getById(
+    Future<RecordDbModel> Function() getRecord,
+  ) async {}
+
+  @override
+  Stream<List<RecordModel>> watchAll() {
+    // TODO: implement watchAll
+    throw UnimplementedError();
+  }
+}
