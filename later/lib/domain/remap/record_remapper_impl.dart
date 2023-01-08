8de5c8421cff59c0a497d94948e6a549c44b2617
Dima Kharitonov <hardi.programm@gmail.com>
2023-01-08 21:27:01 +0200

    added annotation & change key word to implements

--- later/lib/domain/remap/record_remapper_impl.dart
+++ later/lib/domain/remap/record_remapper_impl.dart
@@ -1,8 +1,10 @@
+import 'package:injectable/injectable.dart';
 import 'package:later/database/models/record_model.dart';
 import 'package:later/domain/models/record_model.dart';
 import 'package:later/domain/remap/record_remapper.dart';
 
-class RecordRemapperImpl extends RecordRemapper {
+@LazySingleton(as: RecordRemapper)
+class RecordRemapperImpl implements RecordRemapper {
   @override
   RecordModel dataToDomain(RecordDbModel recordDbModel) {
     return RecordModel(
