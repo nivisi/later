91139a33c765b22ceb6f70f348eb76930dee3e79
Dima Kharitonov <hardi.programm@gmail.com>
2023-01-08 21:16:49 +0200

    added annotation for injectable

--- later/lib/domain/usecases/watch_all_records_use_case.dart
+++ later/lib/domain/usecases/watch_all_records_use_case.dart
@@ -1,6 +1,8 @@
+import 'package:injectable/injectable.dart';
 import 'package:later/domain/repository/record_repository.dart';
 import '../models/record_model.dart';
 
+@lazySingleton
 class WatchAllRecordsUseCase {
   WatchAllRecordsUseCase(this.recordsRepository);
 
