d6e4e9cb83f4e17fe7263f336d7ed99223ca7fd0
Dima Kharitonov <hardi.programm@gmail.com>
2023-01-17 22:42:58 +0200

    create delete & update methods on domain

--- later/lib/domain/repository/record_repository.dart
+++ later/lib/domain/repository/record_repository.dart
@@ -1,11 +1,15 @@
+import 'package:later/database/models/record_model.dart';
+
 import '../models/record_model.dart';
 
 abstract class RecordsRepository {
-  Stream<List<RecordModel>> watchAll();
-  Future<RecordModel?> getById(int id);
   Future<RecordModel> create({
     required String url,
     required String title,
     String? description,
   });
+  Future<RecordModel?> update(RecordDbModel recordDbModel);
+  Stream<List<RecordModel>> watchAll();
+  Future<RecordModel?> getById(int id);
+  Future<String> delete(int id);
 }
