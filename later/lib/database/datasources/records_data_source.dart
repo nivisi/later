9967e73be5d51a30ad508594c7a6aec916109962
Dima Kharitonov <hardi.programm@gmail.com>
2023-01-17 22:45:25 +0200

    create update & delete methods on database

--- later/lib/database/datasources/records_data_source.dart
+++ later/lib/database/datasources/records_data_source.dart
@@ -3,11 +3,13 @@ import 'package:later/database/models/record_model.dart';
 abstract class RecordsDataSource {
   const RecordsDataSource();
 
-  Stream<List<RecordDbModel>> watchAll();
   Future<RecordDbModel> create({
     required String url,
     required String title,
     String? description,
   });
+  Future<RecordDbModel> update(RecordDbModel recordDbModel);
   Future<RecordDbModel?> getById(int id);
+  Stream<List<RecordDbModel>> watchAll();
+  Future<String> delete(int id);
 }
